23bab50241b972fc70fc9f02e6b67f5a8dc0279e
Agamdeep Singh <agamdeep579@gmail.com>
2021-11-29 16:15:52 +0530

    Added the file with the bot

---
+++ main.py
@@ -0,0 +1,14 @@
+# Python Band Name Generator built by Agamdeep Singh
+# Youtube: CodeWithAgam
+# Github: CodeWithAgam
+# Instagram: @agamdeep_21, @coderagam001
+
+# Print the welcome message
+print("Welcome to Band Name Generator built in Python")
+
+# Get the inputs from the user
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+
+# Combine the inputs and print a result
+print(f"Your band name could be {city_name} {pet_name}")
